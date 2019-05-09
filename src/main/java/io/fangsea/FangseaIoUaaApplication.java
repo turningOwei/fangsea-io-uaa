e4b5703dfbe26fad0440c6f9a30e143c3506e019
peng.wei <peng.wei@fangsea.io>
2019-05-09 14:02:24 +0800

    跨域

--- src/main/java/io/fangsea/FangseaIoUaaApplication.java
+++ src/main/java/io/fangsea/FangseaIoUaaApplication.java
@@ -3,6 +3,10 @@ package io.fangsea;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 /**
  * @author turningOwei
@@ -15,4 +19,20 @@ public class FangseaIoUaaApplication {
         SpringApplication.run(FangseaIoUaaApplication.class, args);
     }
 
+    /**
+     * 解决前后端分离跨域问题
+     *
+     * @return
+     */
+    @Bean
+    public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
